ac60631de24d51c7aeb5fc73cfc5bfc506fe5b85
iamsatyamyadav <iamsatyam2002@gmail.com>
2023-08-10 18:56:49 +0530

    Application port 7777

---
+++ BlogWala_App/BlogWalaApp-API/src/main/java/com/iamsatyam/BlogWala/BlogWalaAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamsatyam.BlogWala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWalaAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWalaAppApiApplication.class, args);
+	}
+
+}
---
+++ BlogWala_App/BlogWalaApp-API/src/test/java/com/iamsatyam/BlogWala/BlogWalaAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamsatyam.BlogWala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWalaAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
